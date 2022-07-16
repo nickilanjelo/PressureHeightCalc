8b506838b5ff9a23ea1c24871599c6a7d21feb75
nickilanjelo <nikita.ilyasoff@gmail.com>
2022-07-16 16:43:38 +0300

    * refactored Decimal usage * move pressure converting const to a variable

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 from math import exp
 from decimal import Decimal
 
-PRESSURE = Decimal(760)
-G_BOOST = Decimal(9.81)
-AIR_MASS = Decimal(0.029)
-GAS_CONST = Decimal(8.31)
+PRESSURE = 760
+G_BOOST = 9.81
+AIR_MASS = 0.029
+GAS_CONST = 8.31
+PRESSURE_CONST = 133.33
 
 
 def calc_height_internal(h, t):
-    x = Decimal(- AIR_MASS * G_BOOST * h) / Decimal(GAS_CONST * to_kelvin(t))
+    x = (- AIR_MASS * G_BOOST * h) / (GAS_CONST * to_kelvin(t))
     calculated = Decimal(to_pascal(PRESSURE)) * Decimal(exp(x))
-    return Decimal(from_pascal(calculated))
+    return from_pascal(calculated)
 
 
 def to_pascal(pressure_mm):
-    return pressure_mm * Decimal(133.3)
+    return pressure_mm * PRESSURE_CONST
 
 
 def from_pascal(pascal):
-    return pascal / Decimal(133.33)
+    return Decimal(pascal) / Decimal(PRESSURE_CONST)
 
 
-def to_kelvin(temp):
-    return 273 + temp
+def to_kelvin(celsius):
+    return 273 + celsius
 
 
 height = int(input('Введите высоту: '))
 temp = int(input('Введите температуру: '))
-print(calc_height_internal(Decimal(height), Decimal(temp)))
+print(calc_height_internal(height, temp))
